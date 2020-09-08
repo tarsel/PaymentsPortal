2aa0c3ff21a4d09685c9999a3bc734ce72c9c15c
tarsel <michaelosimbo@yahoo.com>
2020-09-08 17:42:43 +0300

    Commit from VS

---
+++ PaymentsFrontEnd/Models/DateRangeRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PaymentsFrontEnd.Models
+{
+    public class DateRangeRequest
+    {
+        public string MSISDN { get; set; }
+        public string ShortCode { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+    }
+}
--- PaymentsFrontEnd/ViewTransactions.aspx.cs
+++ PaymentsFrontEnd/ViewTransactions.aspx.cs
@@ -13,8 +13,8 @@ namespace PaymentsFrontEnd
 {
     public partial class ViewTransactions : System.Web.UI.Page
     {
-        readonly string baseUrl = "http://197.248.0.20:7329/";
-        //readonly string baseUrl = "http://localhost:7329/";
+        //readonly string baseUrl = "http://197.248.0.20:7329/";
+        readonly string baseUrl = "http://localhost:7329/";
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -60,7 +60,21 @@ namespace PaymentsFrontEnd
 
         protected void btnSubmit_Click(object sender, EventArgs e)
         {
+            string str = txtPhoneNumber.Text;
+            string phoneNumber = "254" + str.Substring(str.Length - 9);
+            string startDate = DateTime.Parse(txtStartDate.Text).ToString("yyyyMMddHHmmss");
+            string endDate = DateTime.Parse(txtEndDate.Text).ToString("yyyyMMddHHmmss");
 
+            ClientSetting clientSetting = GetClientSettingByClientId(long.Parse(ddlClient.SelectedValue));
+
+            if (ddlTransactionType.SelectedItem.Text == "C2B")
+            {
+                GetC2BTransactionsByMsisdnShortCodeAndDate(phoneNumber, clientSetting.C2B, startDate, endDate);
+            }
+            else if (ddlTransactionType.SelectedItem.Text == "STK")
+            {
+                ViewStkByMsidnAndDate(phoneNumber, startDate, endDate);
+            }
         }
 
         public ClientSetting GetClientSettingByClientId(long clientId)
@@ -150,33 +164,39 @@ namespace PaymentsFrontEnd
             gvStk.Visible = true;
         }
 
-        //public void ViewAllC2BRequests()
-        //{
-        //    List<C2BQueryResponse> c2BQueryResponse;
+        public void GetC2BTransactionsByMsisdnShortCodeAndDate(string msisdn, string shortCode, string startDate, string endDate)
+        {
+            List<C2BQueryResponse> c2BQueryResponse;
 
-        //    using (var client = new HttpClient())
-        //    {
-        //        client.BaseAddress = new Uri(baseUrl);
-        //        try
-        //        {
-        //            var response = client.GetAsync("api/Mpesa/GetAllC2BTransactions").Result;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+                try
+                {
+                    DateRangeRequest lipaNaMpesa = new DateRangeRequest { ShortCode = shortCode, MSISDN = msisdn, EndDate = endDate, StartDate = startDate };
 
-        //            if (response.IsSuccessStatusCode)
-        //            {
-        //                c2BQueryResponse = response.Content.ReadAsAsync<List<C2BQueryResponse>>().Result;
+                    client.BaseAddress = new Uri(baseUrl);
 
-        //                gvTransactions.DataSource = c2BQueryResponse;
-        //                gvTransactions.DataBind();
-        //            }
-        //            else
-        //                lblMessage.Text = "System cannot fetch data.";
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            lblMessage.Text = ex.Message;
-        //        }
-        //    }
-        //}
+                    var response = client.PostAsJsonAsync("api/Mpesa/GetC2BQueryByMsisdnAndCode", lipaNaMpesa).Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        c2BQueryResponse = response.Content.ReadAsAsync<List<C2BQueryResponse>>().Result;
+
+                        gvC2B.DataSource = c2BQueryResponse;
+                        gvC2B.DataBind();
+                    }
+                    else
+                        lblMessage.Text = "System cannot fetch data.";
+                }
+                catch (Exception ex)
+                {
+                    lblMessage.Text = ex.Message;
+                }
+            }
+            gvC2B.Visible = true;
+            gvStk.Visible = false;
+        }
 
         public void GetC2BTransactionsByMsisdnAndShortCode(string msisdn, string shortCode)
         {
@@ -262,6 +282,41 @@ namespace PaymentsFrontEnd
                     client.BaseAddress = new Uri(baseUrl);
 
                     var response = client.PostAsJsonAsync("api/Mpesa/GetStkRequestByMsisdn", lipaNaMpesa).Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        lNMStkResponse = response.Content.ReadAsAsync<List<LNMStkResponse>>().Result;
+
+                        gvStk.DataSource = lNMStkResponse;
+                        gvStk.DataBind();
+                    }
+                    else
+                        lblMessage.Text = "System cannot fetch data.";
+                }
+                catch (Exception ex)
+                {
+                    lblMessage.Text = ex.Message;
+                }
+            }
+            gvStk.Visible = true;
+            gvC2B.Visible = false;
+        }
+
+
+        public void ViewStkByMsidnAndDate(string msisdn, string startDate, string endDate)
+        {
+            List<LNMStkResponse> lNMStkResponse;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+                try
+                {
+                    DateRangeRequest lipaNaMpesa = new DateRangeRequest { StartDate = startDate, EndDate = endDate, MSISDN = msisdn };
+
+                    client.BaseAddress = new Uri(baseUrl);
+
+                    var response = client.PostAsJsonAsync("api/Mpesa/GetStkRequestByMsisdnAndDate", lipaNaMpesa).Result;
 
                     if (response.IsSuccessStatusCode)
                     {
